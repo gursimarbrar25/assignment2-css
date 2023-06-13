5b2bce1af2b4711323c752cd4bbd43b704f88b22
gursimarbrar25 <136515172+gursimarbrar25@users.noreply.github.com>
2023-06-13 16:59:29 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function Lyrics() {
+      var lyrics = document.getElementById('lyrics');
+      if (lyrics.style.display === 'none') {
+        lyrics.style.display = 'block';
+      } else {
+        lyrics.style.display = 'none';
+      }
+    }
